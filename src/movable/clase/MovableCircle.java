6d11f19a5ea043ed48dee5b6d7d901c36b97fb1e
basket777 <olivaalexandru@yahoo.com>
2017-11-14 13:36:16 +0200

    5th commit

--- src/movable/clase/MovableCircle.java
+++ src/movable/clase/MovableCircle.java
@@ -8,49 +8,54 @@ import movable.exceptions.YoutOfPlainExc;
 import movable.interfata.Movable;
 
 public class MovableCircle implements Movable {
-	private	int radius;
+	private int radius;
 	private MovablePoint center;
 
 	public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius)
 			throws XoutOfPlainExc, YoutOfPlainExc, SpeedExc, RadiusExc {
-		
-   // trebuie un check ca cercul sa fie in plan si fara constrangeri  
-	
-		
-		if (radius < 0 || radius == 0 ) {
+
+		// trebuie un check ca cercul sa fie in plan si fara constrangeri
+
+		if (check(radius, x, y) == false) {
 			throw new RadiusExc("raza trebuie sa fie pozitiva");
 
-		} 
-		else if ( (radius+x) > PLAIN_X_MAX  || (x-radius)< PLAIN_X_MIN || (radius + y)> PLAIN_Y_MAX || (y- radius)< PLAIN_Y_MIN)
-		{
-			throw new  RadiusExc(" cercul este mai mare decat planul");
-		}
-		else {
+		} else if (check(radius, x, y) == false) {
+			throw new RadiusExc(" cercul este mai mare decat planul");
+		} else if (check(radius, x, y)) {
 			this.center = new MovablePoint(x, y, xSpeed, ySpeed);
 			this.radius = radius;
 
 		}
 	}
 
-	public MovableCircle(MovablePoint center, int radius) {
-		this.center = center;
-		this.radius = radius;
-
-	}
-
 	public int getRadius() {
 		return radius;
 	}
 
-	public void setRadius(int radius) throws RadiusExc {
+	public void setRadius(int radius, int x, int y) throws RadiusExc {
 
-		if (radius < 0 || radius == 0) {
+		if (check(radius, x, y) == false) {
 			throw new RadiusExc("raza trebuie sa fie pozitiva");
 
-		} else {
+		} else if (check(radius, x, y) == false) {
+			throw new RadiusExc("cercul nu se afla in plan");
+		}
+
+		else if (check(radius, x, y)) {
 			this.radius = radius;
 
 		}
+	}
+
+	public boolean check(int radius, int x, int y) throws RadiusExc {
+		boolean i;
+		if (radius < 0 || radius == 0 || (radius + x) > PLAIN_X_MAX || (x - radius) < PLAIN_X_MIN
+				|| (radius + y) > PLAIN_Y_MAX || (y - radius) < PLAIN_Y_MIN) {
+			i = false;
+		} else {
+			i = true;
+		}
+		return i;
 	}
 
 	public MovablePoint getCenter() {
@@ -115,7 +120,7 @@ public class MovableCircle implements Movable {
 			throw new XoutOfPlainExc("X e in afara planului ");
 		} else if (this.getCenterY() > PLAIN_Y_MAX || this.getCenterY() < PLAIN_Y_MIN) {
 			throw new YoutOfPlainExc("Y e in afara planului");
-		} else {
+		} else if (howMuch > 0) {
 			int y = this.getCenterY();
 			y += howMuch;
 			this.setCenterY(y);
@@ -131,7 +136,7 @@ public class MovableCircle implements Movable {
 			throw new XoutOfPlainExc("X e in afara planului ");
 		} else if (this.getCenterY() > PLAIN_Y_MAX || this.getCenterY() < PLAIN_Y_MIN) {
 			throw new YoutOfPlainExc("Y e in afara planului");
-		} else {
+		} else if (howMuch > 0) {
 			int y = this.getCenterY();
 			y -= howMuch;
 			this.setCenterY(y);
@@ -146,7 +151,7 @@ public class MovableCircle implements Movable {
 			throw new XoutOfPlainExc("X e in afara planului ");
 		} else if (this.getCenterY() > PLAIN_Y_MAX || this.getCenterY() < PLAIN_Y_MIN) {
 			throw new YoutOfPlainExc("Y e in afara planului");
-		} else {
+		} else if (howMuch > 0) {
 			int x = this.getCenterX();
 			x -= howMuch;
 			this.setCenterX(x);
@@ -161,11 +166,15 @@ public class MovableCircle implements Movable {
 			throw new XoutOfPlainExc("X e in afara planului ");
 		} else if (this.getCenterY() > PLAIN_Y_MAX || this.getCenterY() < PLAIN_Y_MIN) {
 			throw new YoutOfPlainExc("Y e in afara planului");
-		} else {
+		} else if (howMuch > 0) {
 			int x = this.getCenterX();
 			x += howMuch;
 			this.setCenterX(x);
 		}
+		
+		
 	}
+
+	
 
 }
